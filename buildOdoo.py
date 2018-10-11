466d4ca4f9d3c84dfa62fd432f9f0c63a3d74f23
longdt <duongthanhlong93@gmail.com>
2018-10-11 14:54:17 +0700

    ....

--- buildOdoo.py
+++ buildOdoo.py
@@ -2,12 +2,12 @@
 
 import subprocess
 import argparse
+import time
+import logging
 
-PROJECT_ROOT = "/opt/Projects"
-ODOO_PATH = "/opt/Projects/baskin/odoo9_baskin/odoo.py"
-CONFIG_PATH = "/opt/Projects/baskin/config/baskin_staging.conf"
-DATABASE = "GS_ERP_P1_GOLIVE_STAGING_29_05"
-
+logging.basicConfig(format='[%(asctime)s]/%(levelname)s/%(name)s: %(message)s')
+logger = logging.getLogger("Baskin service")
+logger.setLevel(logging.INFO)
 parser = argparse.ArgumentParser()
 
 
@@ -23,26 +23,49 @@ def delete_old_build(project_path):
 
 def restart_service(args):
     # Just to be sure ....
-    assert not not args.project, 'Project name was not supplied'
-    assert not not args.branch, 'Branch name was not supplied'
-    project_path = PROJECT_ROOT + '/' + args.project + '/' + args.branch
-    modules = get_to_upgrade_modules(project_path, args.branch)
-    # delete_old_build(project_path)
 
+    assert not not args.branch, 'Branch name was not supplied !'
+    assert not not args.path, "Project path was not supplied !"
+    modules = get_to_upgrade_modules(args.path, args.remote, args.branch)
+    logger.info("Modules to upgrade: %s" % ",".join(modules))
+
+    # Pull new code
+    git_pull(args.path, args.branch)
+
+    if args.tool == 'tmux':
+        tmux_restart(modules, args)
+    elif args.tool == 'supervisor':
+        supervisorctl_restart(modules, args)
+
+def supervisorctl_restart(modules, args):
+    pass
+
+
+def tmux_restart(modules, args):
+    restart_command = [args.executable, '-c', args.config, '--load=web,web_kanban,connector']
+    logger.info("Stopping current service")
+    # Stop current service
+    subprocess.call([
+        'pkill',
+        '-f',
+        " ".join(restart_command)
+    ])
+
+    logger.info("Killing old tmux session ")
     # Kill old tmux session
-    try:
-        subprocess.call([
-            'tmux',
-            'kill-session',
-            '-t',
-            args.branch
-        ])
-    except Exception as e:
-        pass
+    subprocess.call([
+        'tmux',
+        'kill-session',
+        '-t',
+        args.branch
+    ])
 
     # Restart service
-    restart_command = [args.executable, '-c', args.config, '-u', ','.join(modules)]
+    if modules and args.database:
+        restart_command.extend(['-u', ','.join(modules), '-d', args.database])
 
+    logger.info("Starting service ...")
+    # Create new session
     subprocess.call([
         'tmux',
         'new',
@@ -51,6 +74,7 @@ def restart_service(args):
         args.branch
     ])
 
+    # Send command to tmux session
     subprocess.call([
         'tmux',
         'send',
@@ -61,17 +85,45 @@ def restart_service(args):
     ])
 
 
-def get_to_upgrade_modules(project_path, branch):
+def git_pull(path, branch):
+    logger.info("Pulling new code from %s" % branch)
+    subprocess.call([
+        'git',
+        'reset',
+        '--hard'
+    ], cwd=path)
+
+    subprocess.call([
+        'git',
+        'checkout',
+        branch
+    ], cwd=path)
+
+    subprocess.call([
+        'git',
+        'pull',
+        '--rebase',
+    ], cwd=path)
+
+
+def get_to_upgrade_modules(project_path, remote, branch):
     """
 
     :return: (list) modules that will be upgrade in this build
     """
     to_upgrades = set([])
+    subprocess.call([
+        'git',
+        'remote',
+        'update',
+        remote,
+        '--prune'
+    ])
     recent_changes = subprocess.check_output([
         'git',
         'diff',
-        'HEAD^1',
-        branch,
+        'HEAD',
+        remote + '/' + branch,
         '--name-only',
     ], cwd=project_path)
 
@@ -85,14 +137,18 @@ def get_to_upgrade_modules(project_path, branch):
 
 
 def init_arguments():
-    parser.add_argument('--project', help='Project name', required=True)
+    parser.add_argument('--path', help='Path to project', required=True)
     parser.add_argument('--branch', help='Branch name', required=True)
     parser.add_argument('--tool', help='Odoo Service management tool', default='tmux', choices=('tmux', 'supervisor', 'systemd'))
-    parser.add_argument('--executable', help='Path to odoo executable file', default=ODOO_PATH)
-    parser.add_argument('--config', help='Path to project config file', default=CONFIG_PATH)
+    parser.add_argument('--executable', help='Path to odoo executable file', required=True)
+    parser.add_argument('--config', help='Path to project config file', required=True)
+    parser.add_argument('--database', help='Database name')
+    parser.add_argument('--remote', help='Git remote', default='origin')
 
 
 if __name__ == '__main__':
     init_arguments()
     args = parser.parse_args()
     restart_service(args)
+
+# /opt/scripts/autotest.py --project=${JOB_NAME} --suites=Discount
